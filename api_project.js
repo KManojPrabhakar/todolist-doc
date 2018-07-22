002ad31c790269bcce188204377e4ff21445621c
Manoj Prabhakar <manojprabhakar675@gmail.com>
2018-07-22 18:16:59 +0530

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "TodoList Application",
+  "version": "0.0.1",
+  "description": "Documentation for the TodoList App",
+  "title": "TodoList APIs",
+  "url": "http://todolist-backend.testmanoj.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-07-22T12:41:11.315Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
